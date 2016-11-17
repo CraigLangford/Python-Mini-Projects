3332b39e20856061393124b8b1a481cc1cd2d3ea
CraigLangford <craigllangford@gmail.com>
2016-11-17 23:56:54 +0000

    Corrected Entrys to entries and changed entry name to entry title

--- Test-Driven-Django-Development/myblog/blog/tests.py
+++ Test-Driven-Django-Development/myblog/blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryModelTest(TestCase):
+
+    def test_string_representation(self):
+        """ Ensure that the entry has a string representation """
+        entry = Entry(title="My entry title")
+        self.assertEqual(str(entry), "My entry title")
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
