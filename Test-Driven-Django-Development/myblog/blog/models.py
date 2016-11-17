0b52e554435d29bef8d163671be32af862ad7b6e
CraigLangford <craigllangford@gmail.com>
2016-11-17 22:35:06 +0000

    Added new blog project, added it to settings and created an blog entry model.

---
+++ Test-Driven-Django-Development/myblog/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Entry(models.Model):
+    """ Model for blog entry """
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey('auth.User')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
