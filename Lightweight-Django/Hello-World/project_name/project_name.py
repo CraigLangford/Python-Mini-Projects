92c87a2f109b03e760b1ea21a904d6678374f8ab
CraigLangford <craigllangford@gmail.com>
2017-01-29 15:04:50 +0000

    Used project_name as a template to create a foo project

--- Lightweight-Django/Hello-World/project_name/project_name.py
+++ Lightweight-Django/Hello-World/project_name/project_name.py
@@ -1,12 +1,18 @@
+import os
 import sys
-
 
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY='lightweightdjangoprojectsecretkey',
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS = ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
@@ -17,6 +23,7 @@ settings.configure(
 
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 
@@ -29,6 +36,8 @@ urlpatterns = (
     url(r'^$', index),
 )
 
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
