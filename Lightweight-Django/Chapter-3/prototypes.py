0df72cec542a5774399dc13ddf45dcb96e6dd3ec
CraigLangford <craigllangford@gmail.com>
2017-02-03 10:36:13 +0000

    Moved views to correct location.

--- Lightweight-Django/Chapter-3/prototypes.py
+++ Lightweight-Django/Chapter-3/prototypes.py
@@ -2,35 +2,7 @@ import os
 import sys
 
 from django.conf import settings
-from django.http import Http404
-from django.shortcuts import render
-from djang.template import Template
-from django.utils._os import safe_join
 
-
-def get_page_or_404(name):
-    """ Return page content as a Django template or raise 404 error. """
-    try:
-        file_path = safe_join(settings.SITE_PAGES_DIRECTORY, name)
-    except ValueError:
-        raise Http404('Page Not Found')
-    else:
-        if not os.path.exists(file_path):
-            raise Http404('Page Not Found')
-    with open(file_path, 'r') as f:
-        page = Template(f.read())
-
-    return page
-
-def page(request, slug='index'):
-    """ Render the request page if found. """
-    file_name = '{}.html'.format(slug)
-    page = get_page_or_404(file_name)
-    context = {
-        'slug': slug,
-        'page': page,
-    }
-    return render(request, 'page.html', context)
 
 BASE_DIR = os.path.dirname(__file__)
 
