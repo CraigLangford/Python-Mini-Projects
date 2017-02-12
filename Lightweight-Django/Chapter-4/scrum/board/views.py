cf4b3ccbcd1504d2a8001f86e400340fbdc2d7ff
CraigLangford <craigllangford@gmail.com>
2017-02-12 00:38:00 +0000

    Created serializers for tasks and users

--- Lightweight-Django/Chapter-4/scrum/board/views.py
+++ Lightweight-Django/Chapter-4/scrum/board/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import authentication, permissions, viewsets
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+User = get_user_model()
 
 
 class DefaultsMixin(object):
@@ -25,3 +29,19 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+
+class = TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """ API endpoint for listing and creating tasks. """
+
+    queryset = Task.objects.all()
+    serializers_class = TaskSerializer
+
+
+class = UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+    """ API endpoint for listing users. """
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
